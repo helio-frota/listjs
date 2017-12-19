16491735c93c4783125b18ea767b5c3ef874e0bf
Helio Frota <00hf11@gmail.com>
2017-12-19 11:04:06 -0300

    src: first commit

---
+++ test/listjs.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const listJS = require('../index');
+
+test('two JS files found.', () => {
+  expect.assertions(1);
+  return listJS('.')
+    .then(data => {
+      expect(data).toEqual(['index.js', 'test/listjs.test.js']);
+    })
+    .catch(e => expect(e).toMatch('error'));
+});
+
+test('one JS file found (ignoring test directory).', () => {
+  expect.assertions(1);
+  return listJS('.', ['test'])
+    .then(data => {
+      expect(data).toEqual(['index.js']);
+    })
+    .catch(e => expect(e).toMatch('error'));
+});
+
+test('the search fails with an error.', () => {
+  expect.assertions(1);
+  return listJS('', {})
+  .catch(e => expect(e.toString())
+  .toMatch('Error: ENOENT: no such file or directory, scandir \'\''));
+});
